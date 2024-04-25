c15c381e7ac214d5d1253d4bc8a16c913bb56ab9
Phanhpham <147391372+Phanhpham@users.noreply.github.com>
2024-04-25 19:32:55 +0700

    Add files via upload

---
+++ session 18/b6.js
@@ -0,0 +1,28 @@
+"use strict";
+/**
+ Abstract Class:
+Abstract class là một lớp trừu tượng không thể được khởi tạo trực tiếp, mà chỉ được sử dụng để tạo ra các lớp con.
+Abstract class có thể chứa phương thức trừu tượng và phương thức có cài đặt.
+Các lớp con phải triển khai (implement) tất cả các phương thức trừu tượng của abstract class hoặc đánh dấu chính lớp đó là một abstract class.
+*/
+// Ví dụ:
+class Animal {
+    move() {
+        console.log("Moving...");
+    }
+}
+class Dog extends Animal {
+    makeSound() {
+        console.log("Woof!");
+    }
+}
+const dog = new Dog();
+dog.makeSound(); // Output: Woof!
+dog.move(); // Output: Moving...
+/**
+ Interface:
+Interface là một cấu trúc trừu tượng mà chỉ định các phương thức và thuộc tính mà một đối tượng phải triển khai.
+Interface không chứa cài đặt của phương thức, chỉ định tên và kiểu của chúng.
+Một lớp có thể triển khai nhiều interface.
+
+ */
---
+++ session 18/index.js
@@ -0,0 +1,35 @@
+"use strict";
+/*
+    interface:giao diện
+    dùng để dinh nghĩa cac thuoc tính và phương thưc các lớp con kế thừa bắt buộc phải triển khai
+    ==> cũng giống như abstract
+    - có những điểm gì khác nhau so vs abstract
+    - để tạo abstract thì dùng tu khôas abstract
+    - để tạo interface thì dùng interface
+    -trong interface se gồm có thuoc tính va phương thức
+    - đối vs phương thức thì ko co phần thân tức la nó chỉ định nghĩa thôi
+    - đối với interface thì ko thể dùng kiểu ket hợp (union và intersection)
+    - có thể khới tạo đối tượng từ interface ko?
+    ==> ko the khoi tạo doi tượng từ interface
+    - interface sinh ra nhằm mục đích gì ??
+    ==> - interface la 1 cái khuôn mẫu cho các class khác kế thừa phải tuân theo
+    - các class con khi kế thừa interface băt buoc phải trien khai các thuoc tinh va phương thức cua interface(implement)
+    - 1 class có the trien khai nhieu interface k?
+    ==> được
+    - interface có ke thừa interface dc ko?
+    ==> có thể ke thua
+    - trong interface thì ko thể đăt access modifier đc
+ */
+class Person111 {
+    constructor(name, address) {
+        this.name = name;
+        this.address = address;
+    }
+    // bat buoc phai trien khai pthuc sale trong interface
+    sale() {
+        return "giam gia 5%";
+    }
+    sale1() {
+        return "giam gia 15%";
+    }
+}
